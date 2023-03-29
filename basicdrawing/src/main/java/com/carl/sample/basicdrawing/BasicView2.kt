7346640da0aabc20f46cbd72be7c23fbf6a7edbb
Carl <winstone.su@gmail.com>
2023-03-29 16:03:36 +0800

    feat(basic): 增加基础绘制图形代码

--- basicdrawing/src/main/java/com/carl/sample/basicdrawing/BasicView2.kt
+++ basicdrawing/src/main/java/com/carl/sample/basicdrawing/BasicView2.kt
@@ -13,7 +13,7 @@ import android.view.View
  * @Author : AppOS
  * @Description :
  */
-class BasicView: View {
+class BasicView2: View {
     constructor(context: Context): super(context)
 
     constructor(context: Context, atts: AttributeSet): super(context,atts)
@@ -28,14 +28,26 @@ class BasicView: View {
         paint.run {
             //设置画笔颜色
             color = Color.RED
-            //设置画笔填充样式
-            style = Paint.Style.FILL
             //设置画笔宽度
             strokeWidth = 50f
         }
+        //设置画笔填充样式
+        paint.style = Paint.Style.FILL
         //画圆 cx: 圆心x坐标，cy: 圆心y坐标
-        canvas?.drawCircle(190f,200f,150f,paint)
-        paint.color = 0x7EFFFF00
-        canvas?.drawCircle(190f,200f,100f,paint)
+        canvas?.run {
+            drawCircle(190f,200f,100f,paint)
+            paint.style = Paint.Style.STROKE
+            drawCircle(190f,500f,100f,paint)
+            paint.style = Paint.Style.FILL_AND_STROKE
+            drawCircle(190f,800f,100f,paint)
+
+            paint.strokeWidth = 5f
+            paint.style = Paint.Style.FILL
+            paint.textSize = 36f
+            paint.color = Color.BLACK
+            drawText("FILL",160f,350f,paint)
+            drawText("STROKE",130f,660f, paint)
+            drawText("FILL_AND_STROKE",80f,970f,paint)
+        }
     }
 }
